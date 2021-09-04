9ce3fa36b64b7de0f104ad3014cfaacba6b6a72f
rowenci <459287499@qq.com>
2021-09-04 20:38:55 +0800

    pointer

--- 11pointer2.cpp
+++ 11pointer2.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main(){
 
-    
+    // 间接寻址
+
+    int var;
+    int *ptr;
+    int **pptr;
+    var = 3000;
+    ptr = &var;
+    pptr = &ptr;
+
+    cout << var << endl;
+    cout << *ptr << endl;
+    cout << **pptr << endl;
+
     return 0;
 
 }
