8b9ba56024dc61511b32c0a3065438408f44afe6
rowenci <459287499@qq.com>
2021-09-10 20:47:33 +0800

    Update 16oop9.cpp

--- 16oop9.cpp
+++ 16oop9.cpp
@@ -2,9 +2,39 @@
 
 using namespace std;
 
+/*
+C++ 接口是使用抽象类来实现的
+如果类中至少有一个函数被声明为纯虚函数，则这个类就是抽象类。
+纯虚函数是通过在声明中使用 "= 0" 来指定的
+*/
+
+class Shape{
+    protected:
+        int width;
+        int height;
+    public:
+        virtual int getArea() = 0;
+        void setWidth(int width){
+            this->width = width;
+        }
+        void setHeight(int height){
+            this->height = height;
+        }
+};
+
+class Rectangle : public Shape{
+    public:
+        int getArea(){
+            return (width * height);
+        }
+};
+
 int main(){
 
-    
+    Rectangle rect;
+    rect.setHeight(1);
+    rect.setWidth(2);
+    cout << rect.getArea() << endl;
 
     return 0;
 
