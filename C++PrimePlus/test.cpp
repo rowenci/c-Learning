402116484e0ffa0a864d7d7e5a872a0569e21248
rowenci <459287499@qq.com>
2021-10-26 21:34:38 +0800

    Update test.cpp

--- C++PrimePlus/test.cpp
+++ C++PrimePlus/test.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
+    
+    char firstName[10];
+    char lastName[10];
+    char grade;
+    int age;
+    cout << "What is your first name? ";
+    cin.getline(firstName, 10);
+    cout << "What is your last name? ";
+    cin.getline(lastName, 10);
+    cout << "What letter grade do you deserve? ";
+    cin >> grade;
+    cout << "what is your age? ";
+    cin >> age;
+    grade += 1;
+    cout << "Name: " << lastName << "," << firstName << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Age: " << age <<endl;
+
     return 0;
 }
