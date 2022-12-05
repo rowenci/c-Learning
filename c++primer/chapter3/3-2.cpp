efbfe10870658e780015042beeb36eabd028cb56
rowenci <459287499@qq.com>
2022-12-05 14:37:01 +0800

    3-2

---
+++ c++primer/chapter3/3-2.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+#include "map"
+using namespace std;
+int main(){
+    // string is an object
+    string s1;  // 默认初始化一个空字符串
+    string s2 = s1; // s2是s1的副本
+    string s3 = "rowenci";  // s3是该字符串字面值的副本；拷贝初始化
+    string s4(10, 'c'); // s4的内容是10个c；直接初始化
+    string s5("rowenci");   // 直接初始化
+    s5.size();
+    map<int, bool> test;
+    test.
+    return -1;
+}
